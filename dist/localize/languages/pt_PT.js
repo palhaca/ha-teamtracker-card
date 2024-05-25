3e0c13053238d90777b6088e796fdc38a7ba8a6e
Eduardo Martins <71613026+palhaca@users.noreply.github.com>
2024-05-25 14:21:19 +0100

    Create pt_PT.js
    Português de Portugal - Eduardo Martins

---
+++ dist/localize/languages/pt_PT.js
@@ -0,0 +1,148 @@
+export const pt_PT = {
+    "common": {
+        "api_error": "Erro de API",
+        "no_upcoming_games": "Sem novos jogos %s",
+        "finalTerm": "%s - Final",
+        "byeTerm": "Adeus",
+        "tourney2": "Final",
+        "tourney4": "Semi Final",
+        "tourney8": "Quartos de Final",
+        "tourney16": "dezasseis avos",
+        "tourney32": "Trinta e dois avos",
+        "tourney64": "Secenta e quatro avos",
+        "tourney128": "Primeiras Rondas",
+        "tourney256": "Primeiras Rondas",
+        "today": "Hoje",
+        "tomorrow": "Amanhã"
+    },
+    "australian-football": {
+        "startTerm": "Inicio",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Probabilidade de Vento",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "baseball": {
+        "startTerm": "Primeiro Lançamento",
+        "overUnder": "Mais/Menos: %s",
+        "gameStat1": "Bolas %s",
+        "gameStat2": "Strikes %s",
+        "gameStat3": "%s Fora",
+        "gameBar": "Porcentagem de Vitória",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "basketball": {
+        "startTerm": "Inicio",
+        "overUnder": "Mais/Menos: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Porcentagem de Vitória",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "cricket": {
+        "startTerm": "Inicio",
+        "overUnder": "Mais/Menos: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Porcentagem de Vitória",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "football": {
+        "startTerm": "Pontapé de Saida",
+        "overUnder": "Mais/Menos: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Porcentagem de Vitória",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "golf": {
+        "startTerm": "Inicio de Ronda",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Pancadas",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "hockey": {
+        "startTerm": "Inicio",
+        "overUnder": "Mais/Menos: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Chutes no Gol",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "mma": {
+        "startTerm": "Start",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Porcentagem de Vitória",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "racing": {
+        "startTerm": "Arranque",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Voltas",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "soccer": {
+        "startTerm": "Pontapé de Saida",
+        "overUnder": "Mais/Menos: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Chutes no Gol",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "tennis": {
+        "startTerm": "Primeiro Serviço",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "%s",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "volleyball": {
+        "startTerm": "Primeiro serviço",
+        "overUnder": "Mais/Menos: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": " Pontos %s",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "default": {
+        "startTerm": "Inicio",
+        "overUnder": "Mais/Menos: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Porcentagem de Vitória",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    }
+}
